804a83da4af9823efd37a4e2778decbab2c8b29a
AfekC <afek8260@gmail.com>
2020-05-13 10:35:50 +0300

    fixes

--- 2/Main.js
+++ 2/Main.js
@@ -1,27 +1,34 @@
 
-window.onload = function() {
-    time = new Date()
-    this.document.getElementsByClassName("time")[0].innerHTML = getTime()
+window.onload = () => {
+    time = new Date();
+    this.document.getElementsByClassName("time")[0].innerHTML = getTime();
 };
 
-let runner
+let runner;
 
 clicked = () => {
-    let button = this.document.getElementsByClassName("btn")[0]
+    let button = this.document.getElementsByClassName("btn")[0];
 
     if(button.innerHTML === "הפעל"){
-        this.document.getElementsByClassName("time")[0].innerHTML = getTime()
+        this.document.getElementsByClassName("time")[0].innerHTML = getTime();
         runner = setInterval(function(){
-            this.document.getElementsByClassName("time")[0].innerHTML = getTime()
+            this.document.getElementsByClassName("time")[0].innerHTML = getTime();
           }, 1000);
-        button.innerHTML = "עצור"
+        button.innerHTML = "עצור";
     } else {
-        button.innerHTML = "הפעל"
-        clearInterval(runner)
+        button.innerHTML = "הפעל";
+        clearInterval(runner);
     }
 }
 
 getTime = () => {
     time = new Date()
-    return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
+    return padding(time.getHours()) + ":" + padding(time.getMinutes()) + ":" + padding(time.getSeconds());
+}
+
+padding = (number) => {
+    if (number.toString().length === 1){
+        return '0' + number;
+    }
+    return number;
 }
